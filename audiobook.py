359df0217061034c5a0aa4dbac93c0a6233c4324
Shashank Mishra <shashank3oct@gmail.com>
2022-12-07 00:41:55 +0530

    Create audiobook.py

---
+++ audiobook.py
@@ -0,0 +1,20 @@
+'''
+Create AudioBook from any PDF using Python
+Author: Shashank Mishra
+'''
+import pyttsx3
+import PyPDF2
+
+book = open('Gita.pdf','rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+num_pages = pdf_reader.numPages
+
+play = pyttsx3.init()
+print('Playing Audio Book')
+
+for num in range(0, num_pages):
+	page = pdf_reader.getPage(num)
+	data = page.extractText()
+
+	play.say(data)
+	play.runAndWait()
